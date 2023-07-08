77d73f9939e949a826a53c7aa426f7bc86544ef2
tsb26 <tejasvi109bteceai21@igdtuw.ac.in>
2023-07-08 09:20:16 +0530

    commit
    some changes

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+#include<bits/stdc++.h>
+using namespace std;
+void fun(pair<int,int> p){
+    cout<<p.first;
+}
+int main(){
+
+pair< int,int > p1={1,3};
+pair< int,int > p2=make_pair(4,5);
+fun({5,6});
+return 0;
+
+}
